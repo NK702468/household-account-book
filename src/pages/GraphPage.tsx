8104fb034012b2bdaf0c1e67b560eb35a40cc23e
Nao Kameya <nao280523@gmail.com>
2026-02-20 22:31:01 +0900

    add graph

--- src/pages/GraphPage.tsx
+++ src/pages/GraphPage.tsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom"
 import { ROUTES } from "../ROUTES"
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from "recharts"
 
 
 type Props = {}
 
 export default function GraphPage({}: Props) {
+  const data: object[] = [
+    {name: "リンゴ", price: 150},
+    {name: "バナナ", price: 100}
+  ];
+  
   return (
     <div>
-      GraphPage
+      <BarChart width={400} height={300} data={data}>
+        <XAxis dataKey="name"/>
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="price"/>
+      </BarChart>
       <Link to={ROUTES.EXPENSE}>記録へ</Link>
       <Link to={ROUTES.HOME}>ホームページへ</Link>
     </div>
