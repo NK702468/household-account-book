b155c7600d91de0e446ab22890e270ee611822d8
Nao Kameya <nao280523@gmail.com>
2026-02-21 18:11:55 +0900

    change graph

--- src/pages/GraphPage.tsx
+++ src/pages/GraphPage.tsx
@@ -7,18 +7,18 @@ type Props = {}
 
 export default function GraphPage({}: Props) {
   const data: object[] = [
-    {name: "リンゴ", storeA: 150, storeB: 50},
-    {name: "バナナ", storeA: 100, storeB: 30}
+    {month: "1月", sales: 400},
+    {month: "2月", sales: 300},
+    {month: "3月", sales: 500}
   ];
 
   return (
     <div>
       <BarChart width={400} height={300} data={data}>
-        <XAxis dataKey="name"/>
+        <XAxis dataKey="month"/>
         <YAxis />
         <Tooltip />
-        <Bar dataKey="storeA" />
-        <Bar dataKey="storeB" />
+        <Bar dataKey="sales"/>
       </BarChart>
       <Link to={ROUTES.EXPENSE}>記録へ</Link>
       <Link to={ROUTES.HOME}>ホームページへ</Link>
