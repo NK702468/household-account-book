fddecb6db1624073c663102ba53c7addde795ef8
Nao Kameya <nao280523@gmail.com>
2026-02-27 22:32:13 +0900

    fix code

--- server.tsx
+++ server.tsx
@@ -1,9 +1,10 @@
+import { useState } from "react"
 
 
 type Props = {}
 
 export default function server({}: Props) {
-    const [transactions, settTransactions] = useState<>([])
+    const [transactions, settTransactions] = useState<object[]>([])
   return (
     <div>server</div>
   )
